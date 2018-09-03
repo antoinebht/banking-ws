51a1566e4ca06045760d712b381bc7d5479e01b0
BUHOT Antoine <antoine.buhot@fime.com>
2018-09-03 09:08:29 +0200

    Handle CORS

--- ws_root/app.py
+++ ws_root/app.py
@@ -1,8 +1,16 @@
 import falcon
 import json
 
-api = app = falcon.API()
+class CorsMiddleware(object):
+    def process_request(self, request, res):
+        res.set_header('Access-Control-Allow-Origin', '*')
+        res.set_header('Access-Control-Allow-Methods', 'DELETE,GET,HEAD,PATCH,POST,PUT')
+        res.set_header('Access-Control-Expose-Headers', 'auth')
+        res.set_header('Access-Control-Allow-Headers', 'auth')
+
+
+api = app = falcon.API(middleware=[CorsMiddleware()])
+
 
 import antb_bank.app
-
 antb_bank.app.register_app(api)
